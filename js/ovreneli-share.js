388d70dd4edaaa40aa5cc417e95d1b2333a19824
So Nakanishi <so.nakanishi@gmail.com>
2012-02-25 17:46:22 +0900

    JS修正

--- js/ovreneli-share.js
+++ js/ovreneli-share.js
@@ -7,8 +7,7 @@ if (location.hostname === 'ovreneli.github.com') {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-//    js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=248108891944687";
-    js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1";
+    js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=248108891944687";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
